aad52536705332b8de256b62373fab8b7931e01c
Kiazim Khutaba <kiazim.khutaba@yandex.com>
2023-05-15 15:02:53 +0300

    added paginator composer.json library

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace MyBlog\Controllers;
 
+use JasonGrimes\Paginator;
 use MyBlog\Core\Db\DatabaseInterface;
 use MyBlog\Core\Traits\ToJsonStringTrait;
 use MyBlog\Exceptions\ResourceNotFoundException;
@@ -32,19 +33,22 @@ class IndexController extends BaseController
     public function index(Request $request): bool|string
     {
         $posts_per_page = 5;
-        $page = intval($request->query->get('page', 1));
+        $currentPage = intval($request->query->get('page', 1));
         // Todo: refactor negative and zero values for page var
-        $offset = $page === 1 ? 0 : $posts_per_page * ($page - 1);
+        $offset = $currentPage === 1 ? 0 : $posts_per_page * ($currentPage - 1);
 
         $posts = $this->postRepository->getPosts($posts_per_page, $offset);
 
         if(!$posts)
             throw new ResourceNotFoundException();
 
-        $pages = floor($this->postRepository->getCount() / $posts_per_page);
+        $totalItems = $this->postRepository->getCount();
+        //$pages = floor( $totalItems / $posts_per_page);
+
+        $paginator = new Paginator($totalItems, $posts_per_page, $currentPage, '?page=(:num)');
 
         // Todo: not suit for many pages
-        return $this->render('index/index.html.twig', ['posts' => $posts, 'pages' => $pages, 'active_page' => $page]);
+        return $this->render('index/index.html.twig', ['posts' => $posts, 'paginator' => $paginator]);
     }
 
 
